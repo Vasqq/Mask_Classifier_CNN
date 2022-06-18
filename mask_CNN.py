52ed051748a2264eac1e0e1be51b2f9979e195ec
Liam Pereira <pereira.l.h@hotmail.ca>
2022-06-18 14:14:18 -0400

    big changes

--- mask_CNN.py
+++ mask_CNN.py
@@ -9,20 +9,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import plot_confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
+from sklearn.model_selection import train_test_split
 from skorch import NeuralNetClassifier
 from torch.utils.data import random_split
-#from torch.optim import lr_scheduler
 import torch.optim as optim
 
-
-# Device config
-
-#device = 'cpu'
-#device = 'cuda'
-#print(f'Is GPU avaialble?\n {torch.cuda.is_available()}')
-
 #   Hyper parameters
-NUM_EPOCHS = 50
+NUM_EPOCHS = 2
 BATCH_SIZE = 32
 LEARNING_RATE = 0.001
 IMG_SIZE = 32
@@ -33,10 +26,11 @@ transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-#  Get data
+#  Get and split dataset
+dataset = torchvision.datasets.ImageFolder(root = './data', transform=transform)
+train_dataset, test_dataset = train_test_split(dataset, test_size = 0.2)
 
-train_dataset = torchvision.datasets.ImageFolder(root = './data/training', transform=transform)
-test_dataset = torchvision.datasets.ImageFolder(root = './data/test', transform=transform)
+print(len(test_dataset))
 
 # Load data
 
@@ -46,6 +40,23 @@ test_loader = DataLoader(dataset = test_dataset, batch_size = BATCH_SIZE, shuffl
 classes = ('maskless_mask','cloth_mask', 'surgical_mask', 'n95_mask')
 NUM_CLASSES = len(classes)
 
+"""
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+# get some random training images
+dataiter = iter(train_loader)
+images, labels = dataiter.next()
+
+# show images
+imshow(torchvision.utils.make_grid(images))
+# print labels
+print(' '.join(f'{classes[labels[j]]:5s}' for j in range(BATCH_SIZE)))
+"""
 
 # Define model
 
@@ -90,74 +101,6 @@ class ConvNN(nn.Module):
 
 model = ConvNN()
 
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr = LEARNING_RATE)
-
-n_total_steps = len(train_loader)
-loss_list = []
-acc_list = []
-
-#   Training loop
-"""
-
-for epoch in range(NUM_EPOCHS):
-
-    for i, (images,labels) in enumerate(train_loader):
-
-        #   Forward pass
-        outputs = model(images)
-        loss = criterion(outputs,labels)
-        loss_list.append(loss.item())
-
-        #   Backward pass and optimize
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        if(i+1) % 12 == 0:
-            print(f'epoch {epoch + 1} / {NUM_EPOCHS}, step {i + 1}/{n_total_steps}, loss = {loss.item():.4f}')
-"""
-
-
-#FILE = "trained_model.pth"
-#torch.save(model.state_dict(), FILE)
-PATH = './trained_model.pth'
-model = ConvNN()
-model.load_state_dict(torch.load(PATH))
-
-# Testing
-
-with torch.no_grad():
-    n_correct = 0
-    n_samples = 0
-    n_class_correct = [0 for i in range(NUM_CLASSES)]
-    n_class_samples = [0 for i in range(NUM_CLASSES)]
-
-    for images, labels in test_loader:
-        outputs = model(images)
-
-        _, predicted = torch.max(outputs, 1)
-        n_samples += labels.size(0)
-        n_correct += (predicted == labels).sum().item()
-
-        for i in range(BATCH_SIZE):
-            label = labels[i]
-            pred = predicted[i]
-
-            if (label == pred):
-                n_class_correct[label] +=1
-            n_class_samples[label] += 1
-
-    acc = 100.0 * n_correct/n_samples
-    print('\n***TEST DATA***')
-    print(f'\nOverall accuracy = {acc:.3f} %\n')
-
-    for i in range(NUM_CLASSES):
-        acc = 100.0 * n_class_correct[i] / n_class_samples[i]
-        print(f'Accuracy of {classes[i]}: {acc:.3f} %')
-
-
-
 m = len(train_dataset)
 val_size= int(m * 0.2)
 train_size = (m - int(m * 0.2))
@@ -168,11 +111,11 @@ y_train = np.array([y for x, y in iter(train_data)])
 torch.manual_seed(0)
 net = NeuralNetClassifier(
     ConvNN,
-    max_epochs=10,
+    max_epochs=2,
     iterator_train__num_workers=0,
     iterator_valid__num_workers=0,
     lr=1e-3,
-    batch_size=5,
+    batch_size=32,
     optimizer=optim.SGD,
     criterion=nn.CrossEntropyLoss
 )
@@ -187,3 +130,4 @@ print(f"F1 is: {100 * f1_score(y_test,y_pred, average='weighted'):.3f} %")
 
 plot_confusion_matrix(net, test_dataset, y_test.reshape(-1, 1),display_labels= classes)
 plt.show()
+
